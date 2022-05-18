81732f2e36abb97d7aa704177cc51603d5d7531c
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-05-17 18:44:59 -0600

    Add files via upload

---
+++ Javascript/ObjectLessons/DynamicNatureOfObjects/index.js
@@ -0,0 +1,10 @@
+const circle = {
+    radius: 1
+};
+
+circle.color = 'yellow';
+circle.draw = function() {}
+
+// You can delete by typing delete circle.color; or circle.draw;
+
+console.log(circle)
