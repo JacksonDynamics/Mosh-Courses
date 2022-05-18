67818cc18e3fbab4e0213828e2d179c4cc659811
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-05-17 18:01:47 -0600

    Add files via upload

---
+++ Javascript/ObjectLessons/Basics/index.js
@@ -0,0 +1,17 @@
+
+
+// Object-oriended Programming (OOP) 
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisable: true,
+    draw: function() {
+        console.log('draw');
+    }
+}
+
+circle.draw(); // Method when a function is part of an Object
+
