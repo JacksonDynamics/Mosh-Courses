37d75d6b562a4ab3b68e58342682e813769b6ede
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-05-19 01:20:46 -0600

    Add files via upload

---
+++ Javascript/ObjectChallenges/FactoryAndConstructorFunctionsChallenge/index.js
@@ -0,0 +1,24 @@
+// street
+// city
+// zipCode
+// showAddress(address)
+
+
+// Factory Function
+function createAddress(street, city, zipCode) {
+    return {street, city, zipCode}
+};
+
+let address1 = createAddress('Pheasant Ridge Trail', 'Lehi', '84043')
+console.log(address1)
+
+
+// Constructor Function
+function Address(street, city, zipCode) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
+}
+
+let address2 = new Address('Pheasant Ridge Trail', 'Lehi', '84043')
+console.log(address2)
