b403b9f5460e89c550e1fc67e645314814fa6a8e
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-05-19 04:11:45 -0600

    Add files via upload

---
+++ Javascript/ObjectChallenges/BlogPostChallenge/index.js
@@ -0,0 +1,21 @@
+// title
+// body
+// author
+// views
+// comments
+//    (author, body)
+// isLive
+
+const blogPost = {
+    title: 'Eye of the World',
+    body: 'This author is great',
+    author: 'Brandon Sanderson',
+    views: 100,
+    comments: [
+        {author: 'Some Guy', body: 'blah blah'},
+        {author: 'Some Girl', body: 'yeah yeah'}
+    ],
+    isLive: true
+}
+
+console.log(blogPost)
