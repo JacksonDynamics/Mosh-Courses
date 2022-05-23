4c7fd4298709a7a83980bc74a52b91a0f34b8437
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-05-22 22:32:12 -0600

    Add files via upload

--- Javascript/Arrays/Adding & Removing Elements/index.js
+++ Javascript/Arrays/Adding & Removing Elements/index.js
@@ -1,6 +1,10 @@
-const numbers = [3, 4];
+
+const numbers = [1, 2, 3, 4, 5, 6];
 
 console.log(numbers);
+
+// ADDING ELEMENTS TO AN ARRAY
+
 // End
 numbers.push(5, 6);
 
@@ -14,5 +18,21 @@ console.log(numbers);
 // Second arg is the amount of indexes you wish to delete from the starting position.
 // Third arg is what things do you wish to add into this starting position index)
 numbers.splice(2, 0, 'a', 'b')
+
+console.log(numbers);
+
+
+console.log("\n" + "-".repeat(30));
+
+//REMOVING ELEMENTS FROM AN ARRAY
+
+// Removes the FIRST element in our array
+numbers.shift();
+
+console.log(numbers);
+
+// Middle (first arg is the starting index of the [].  
+// Second arg is the amount of indexes you wish to delete from the starting position.
+numbers.splice(2, 1)
 
 console.log(numbers);
