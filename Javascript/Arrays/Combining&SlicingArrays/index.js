8a3f2697effdfffd2f20a3efab53eda2e0a809eb
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-05-22 22:59:38 -0600

    Add files via upload

--- Javascript/Arrays/Combining&SlicingArrays/index.js
+++ Javascript/Arrays/Combining&SlicingArrays/index.js
@@ -8,3 +8,10 @@ console.log(combined)
 // Elements start after and end after (in this case after 2 and after 4) [1,2,3,4,5,6] gives us [3, 4]
 const slice = combined.slice(2, 4);
 console.log(slice)
+
+
+// Using the spread operator
+const altCombined = [...first, ...second];
+
+console.log(altCombined);
+
