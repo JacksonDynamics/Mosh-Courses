6d118f35c98436b37325879381bcd330de210bd1
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-05-22 22:56:03 -0600

    Add files via upload

--- Javascript/Arrays/Combining&SlicingArrays/index.js
+++ Javascript/Arrays/Combining&SlicingArrays/index.js
@@ -1,1 +1,10 @@
 
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+const combined = first.concat(second);
+console.log(combined)
+
+// Elements start after and end after (in this case after 2 and after 4) [1,2,3,4,5,6] gives us [3, 4]
+const slice = combined.slice(2, 4);
+console.log(slice)
