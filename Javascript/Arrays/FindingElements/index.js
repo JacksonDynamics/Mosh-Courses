9631ecabe4766eee715dd3e9ce5809ca284d99f7
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-05-22 22:16:09 -0600

    Add files via upload

--- Javascript/Arrays/FindingElements/index.js
+++ Javascript/Arrays/FindingElements/index.js
@@ -1,3 +1,5 @@
+// FINDING ELEMENTS (Primitives)
+
 const numbers = [1, 2, 3, 4, 3, 2 ,1];
 
 // Will bring back the FIRST index instance that specific element is found
@@ -14,3 +16,26 @@ console.log(numbers.lastIndexOf(1));
 console.log(numbers.indexOf(1) !== -1);
 console.log(numbers.includes(1));
 
+// -------------------------------------------
+
+// FINDING ELEMENTS (Reference Types)
+
+const courses = [
+  {id: 1, name: 'a'},
+  {id: 2, name: 'b'}
+];
+
+// When we want to find the first {} that contains the .name === 'a', it will return the entire {} found
+const course = courses.find(function(course) {
+  return course.name === 'a'
+})
+
+console.log(course)
+
+
+// When we want to return the index # of the first {} that contains the .name === 'b'
+const course2 = courses.findIndex(function(course) {
+  return course.name === 'b'
+})
+
+console.log(course2)
