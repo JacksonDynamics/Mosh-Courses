fb8b080e0f5dd3cce0099bf2f1ea0abb68e0fd12
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-05-22 21:32:20 -0600

    Add files via upload

--- Javascript/Arrays/index.js
+++ Javascript/Arrays/index.js
@@ -1,1 +1,18 @@
+const numbers = [3, 4];
 
+console.log(numbers);
+// End
+numbers.push(5, 6);
+
+console.log(numbers);
+// Beginning
+numbers .unshift(1,2)
+
+console.log(numbers);
+
+// Middle (first arg is the starting index of the [].  
+// Second arg is the amount of indexes you wish to delete from the starting position.
+// Third arg is what things do you wish to add into this starting position index)
+numbers.splice(2, 0, 'a', 'b')
+
+console.log(numbers);
