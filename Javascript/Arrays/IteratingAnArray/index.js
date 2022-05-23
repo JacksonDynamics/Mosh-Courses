e34dc4281102a464ef4e603c9a58b3ddc8ccc771
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-05-23 15:00:07 -0600

    Add files via upload

--- Javascript/Arrays/IteratingAnArray/index.js
+++ Javascript/Arrays/IteratingAnArray/index.js
@@ -1,1 +1,9 @@
 
+const numbers = [1, 2, 3];
+
+for (let number of numbers)
+  console.log(number)
+
+// Iterating through an array has the .forEach option.  This also can 
+// provide an index along with the element being displayed
+numbers.forEach((number, index) =>  console.log(index, number))
