4bbeea3557833512c689f39ac4b3711fa955aa00
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-05-22 22:21:34 -0600

    Add files via upload

--- Javascript/Arrays/ArrowFunctions/index.js
+++ Javascript/Arrays/ArrowFunctions/index.js
@@ -1,1 +1,17 @@
 
+const courses = [
+  {id: 1, name: 'a'},
+  {id: 2, name: 'b'}
+];
+
+// Standard Function vs Arrow Function
+const course = courses.find(function(course) {
+  return course.name === 'a'
+})
+
+
+const course2 = courses.find(course =>  course.name === 'a')
+
+console.log(course2)
+
+
