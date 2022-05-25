689c1ecf0bc827af518cf189493064d478bf843c
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-05-25 15:36:48 -0600

    Create index.js

---
+++ Javascript/Functions/FunctionDeclarationsVSExpressions/index.js
@@ -0,0 +1,14 @@
+// Function Declaration
+function walk() {
+  console.log('walk')
+}
+
+// Anonymous Function Expression.  
+// Alternatively, you can make a Named Function Expression by calling 
+// function walk() {};
+let run = function() {
+  console.log('run')
+};
+let move = run;
+run()
+move()
