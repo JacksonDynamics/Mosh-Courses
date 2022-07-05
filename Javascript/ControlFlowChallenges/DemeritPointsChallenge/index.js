1ecff517f7c7eaf7ac4728c0ff3412523bc345e6
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-07-05 16:49:46 -0600

    Update index.js

--- Javascript/ControlFlowChallenges/DemeritPointsChallenge/index.js
+++ Javascript/ControlFlowChallenges/DemeritPointsChallenge/index.js
@@ -2,16 +2,17 @@ const result = checkSpeed(124)
 console.log(result)
 
 function checkSpeed(speed) {
-    const speedLimit = 70
-    const kmPerPoint = 5
+    const speedLimit = 70;
+    const kmPerPoint = 5;
 
-    if (speed < speedLimit + kmPerPoint)
-        return "Ok"
+    if (speed < speedLimit + kmPerPoint) {
+        console.log('Ok');
+        return;
+    }
 
-    const points = Math.floor((speed - speedLimit) / kmPerPoint)
+    let points = Math.floor((speed - speedLimit) / kmPerPoint);
     if (points >= 12) 
-        return ("License Suspended")
+        console.log('License Suspended')
     else
-        return ('Points', points)
-
+        console.log('Points', points);
 }
