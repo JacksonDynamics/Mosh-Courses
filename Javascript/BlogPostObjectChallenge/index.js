e6b341427e70b856127ded23affd5184cb1a09d0
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-07-07 14:30:28 -0600

    Add files via upload

---
+++ Javascript/BlogPostObjectChallenge/index.js
@@ -0,0 +1,13 @@
+let blogPost = {
+    title: "a",
+    body: "lorem",
+    author: "Scoot",
+    views: 414,
+    comments: [
+        {author: "Jack", body: "Hello"},
+        {author: "Jack", body: "Hello"}
+    ],
+    isLive: true
+}
+
+console.log(blogPost)
