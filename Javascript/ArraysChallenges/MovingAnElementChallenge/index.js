b8833bb99352dfd86468e924ce477bdf114bb20b
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-07-09 22:14:28 -0600

    Update index.js

--- Javascript/ArraysChallenges/MovingAnElementChallenge/index.js
+++ Javascript/ArraysChallenges/MovingAnElementChallenge/index.js
@@ -1,18 +1,20 @@
-const numbers = [1, 2, 3, 4, 5, 6]
+const numbers = [1, 2, 3, 4, 1, 1];
 
-const output = move(numbers, 0, 3);
 
-console.log(output);
-
-function move(array, index, offset) {  
-  const position = (index + offset) 
+function move(array, index, offset) {
+    const position = index + offset;
     if (position >= array.length || position < 0) {
-      console.error('Invalid offset.')
-    return;
-  }
+        console.error('Invalid offset.')
+        return;
+    }
+    
+    const output = [...array];
+    const element = output.splice(index, 1)[0];
+    output.splice(index + offset, 0 , element);
+    return output;
+}
 
-  const output = [...array];
-  const element = output.splice(index, 1)[0];
-  output.splice(position, 0, element);
-  return output;
-};
+
+const output = move(numbers, 0, 1)
+
+console.log(output)
