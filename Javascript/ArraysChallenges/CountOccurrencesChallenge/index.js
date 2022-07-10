5ea4969336e6280d45febad45566133ad8011b82
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-07-09 22:57:25 -0600

    Update index.js

--- Javascript/ArraysChallenges/CountOccurrencesChallenge/index.js
+++ Javascript/ArraysChallenges/CountOccurrencesChallenge/index.js
@@ -1,22 +1,22 @@
 const numbers = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1];
 
-const count = countOccurrences(numbers, 1);
 
-console.log(count);
+function countOccurences(array, searchElement) {
+    // let sum = 0;
+    // for (let n of array) 
+    //     if (n === searchElement)
+    //         sum++
+    //     return sum
+    
+    return array.reduce((accumulator, current) => {
+        const occurrence = (current === searchElement) ? 1 : 0;
+        // If you want to see what is happening under the hood
+        console.log(accumulator, current, searchElement)
+        return accumulator + occurrence;
+    }, 0);
+}
 
-function countOccurrences(array, searchElement) {
-  return array.reduce((accumulator, current) => {
-    const occurrence = (current === searchElement) ? 1 : 0;
-    // If you want to see what is happening under the hood
-    console.log(accumulator, current, searchElement);
-    return accumulator + occurrence;
-  },0);
-};
 
+const count = countOccurences(numbers, 1)
 
-
-// let total = 0;
-// for (let element of array) {
-//   if (searchElement === element) total += 1;
-// }
-// return total
+console.log(count)
