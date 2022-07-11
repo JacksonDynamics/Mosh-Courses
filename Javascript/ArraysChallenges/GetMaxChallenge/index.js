8545f565284a0bf371a99f4ed6c1bc8292caf214
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-07-10 19:58:43 -0600

    Update index.js

--- Javascript/ArraysChallenges/GetMaxChallenge/index.js
+++ Javascript/ArraysChallenges/GetMaxChallenge/index.js
@@ -1,19 +1,21 @@
 const numbers = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1];
 
+function getMax(array) {
+    if (array.length === 0) return undefined;
+
+    // let max = array[0];
+    // for (let m of array) {
+    //     if (m > max) 
+    //     max = m;
+    // }
+    // return max
+
+    return array.reduce((accumulator, current) => 
+        (accumulator > current) ? accumulator : current);
+}        
+
+   
+
 const max = getMax(numbers);
 
 console.log(max);
-
-function getMax(array) {
-  if (array.length === 0) return undefined;
-
-  return array.reduce((accumulator, current) => ( accumulator > current ) ? accumulator : current);
-
-  
-  // let most = array[0];
-  // for (let element of array) 
-  //     if (element > most) 
-  //       most = element;
-  //   return most;
-  
-};
