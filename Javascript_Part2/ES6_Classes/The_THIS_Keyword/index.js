1cf5fd01aaa9940d6eb38eae22f60880ade16a11
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-07-21 22:54:45 -0600

    Add files via upload

---
+++ Javascript_Part2/ES6_Classes/The_THIS_Keyword/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Circle = function() {
+    this.draw = function() { console.log(this); }
+};
+
+const c = new Circle();
+// Method Call
+c.draw()
+
+const draw = c.draw;
+
+// Function Call
+draw();
